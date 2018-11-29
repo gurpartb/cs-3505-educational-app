279bea0c9d60733a1370155e9a013ef37496dccd
Caleb <caleb.r.edwards@gmail.com>
2018-11-29 15:16:19 -0700

    need coordinates

--- QtDogs/spritesheettool.cpp
+++ QtDogs/spritesheettool.cpp
@@ -4,3 +4,27 @@ SpriteSheetTool::SpriteSheetTool()
 {
 
 }
+
+///
+/// \brief SpriteSheetTool::addAnimation
+/// \param x
+/// \param y
+/// \param w
+/// \param h
+/// \param numFrames
+/// \param name
+/// \param path
+///
+///
+void SpriteSheetTool::addAnimation(int x, int y, int w, int h, int numFrames, std::string name, QString path)
+{
+    QImage image(path);
+    std::vector<QImage> frames;
+    for(int i = 0; i < numFrames; i++)
+    {
+        QImage newImage = image.copy(x,y,w,h);
+        frames.push_back(newImage);
+        x += w;
+    }
+    dict.insert(name, frames);
+}
