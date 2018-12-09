5f566649ad0a141904f6056422f1e03a697e6634
AvantAveGarde <coltonlee4407@hotmail.com>
2018-12-08 19:18:46 -0700

    Added dying animations

--- QtDogs/mainwindow.cpp
+++ QtDogs/mainwindow.cpp
@@ -121,17 +121,20 @@ void MainWindow::loadAnimations()
     //adding animation frames
 
     dogPath.loadFromFile("../QtDogs/assets/DogSpriteSheetFinal.png");
-    spriteSheetTool.addAnimation(0,   0,    36, 26,  3, "Dog_Idle",       dogPath);
-    spriteSheetTool.addAnimation(0,   28,   36, 26, 14, "Dog_Sitting",    dogPath);
-    spriteSheetTool.addAnimation(0,   59,   36, 26, 12, "Dog_Barking",    dogPath);
-    spriteSheetTool.addAnimation(0,   138,  36, 26, 22, "Dog_Peeing",     dogPath);
-    spriteSheetTool.addAnimation(15,  166,  36, 26, 4,  "Dog_Peeing",     dogPath);
-    spriteSheetTool.addAnimation(0,   189,  36, 26, 9, "Dog_BeginSleep", dogPath);
-    spriteSheetTool.addAnimation(360, 189,  36, 26, 13, "Dog_Sleeping",   dogPath);
-    spriteSheetTool.addAnimation(122, 215,  36, 24, 9, "Dog_WakeUp",     dogPath);
-    spriteSheetTool.addAnimation(0, 240, 36, 24, 11,    "Dog_Walking",    dogPath);
-    spriteSheetTool.addAnimation(0, 263, 36, 25, 4,     "Dog_Running",    dogPath);
-    spriteSheetTool.addAnimation(0, 316, 36, 55, 10,    "Dog_Flipping",   dogPath);
+    //spriteSheetTool.addAnimation(0,   9,    36, 26,  3, "Dog_Idle",       dogPath);
+    //spriteSheetTool.addAnimation(410,   339, 72, 40,  7, "Dog_Dying",   dogPath);
+    //spriteSheetTool.addAnimation(770, 304, 72, 34,  1, "Dog_Dead",   dogPath);
+    spriteSheetTool.addAnimation(0,   37,   36, 26, 14, "Dog_Sitting",    dogPath);
+    spriteSheetTool.addAnimation(0,   68,   36, 26, 12, "Dog_Barking",    dogPath);
+    spriteSheetTool.addAnimation(0,   147,  36, 26, 22, "Dog_Peeing",     dogPath);
+    spriteSheetTool.addAnimation(15,  175,  36, 26, 4,  "Dog_Peeing",     dogPath);
+    spriteSheetTool.addAnimation(0,   198,  36, 26, 9,  "Dog_BeginSleep", dogPath);
+    spriteSheetTool.addAnimation(360, 198,  36, 26, 13, "Dog_Sleeping",   dogPath);
+    spriteSheetTool.addAnimation(15, 229,  36, 26, 3,   "Dog_Sleeping",   dogPath);
+    spriteSheetTool.addAnimation(122, 224,  36, 24, 9,  "Dog_WakeUp",     dogPath);
+    spriteSheetTool.addAnimation(0, 249, 36, 24, 11,    "Dog_Walking",    dogPath);
+    spriteSheetTool.addAnimation(0, 272, 36, 25, 4,     "Dog_Running",    dogPath);
+    spriteSheetTool.addAnimation(0, 325, 36, 55, 10,    "Dog_Flipping",   dogPath);
 
     splashScreenPath.loadFromFile("../QtDogs/assets/Splash_Screen.png");
     for(int i = 0; i < 7; ++i)
